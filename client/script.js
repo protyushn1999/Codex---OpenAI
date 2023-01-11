c8344443ad7fe8dfba71632bc150e8ecb33775b5
protyushn1999 <protyushn@gmail.com>
2023-01-12 03:08:38 +0530

    added server link in script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:8084', {
+    const response = await fetch('https://codex-8m4y.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
